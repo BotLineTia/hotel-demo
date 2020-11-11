a71903e507564d38fcb56f2c5f70a34fc285909b
BotLineTia <61738808+BotLineTia@users.noreply.github.com>
2020-11-11 10:23:18 +0700

    Update db.php

--- admin/db.php
+++ admin/db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","","hotel") or die(mysql_error());
+$con = mysqli_connect("mysql-15150-0.cloudclusters.net","anh","123456789","hotel","15150") or die(mysql_error());
 
 ?>
